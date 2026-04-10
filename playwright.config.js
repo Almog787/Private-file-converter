6e6c2ec2bd3186eaadc9d93c18eb26630070ad7c
Almog787 <143160546+Almog787@users.noreply.github.com>
2026-04-10 12:41:58 +0300

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -2,16 +2,17 @@
 const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
-  testDir: './test', // התיקייה שבה שמנו את קובץ הבדיקות
+  testDir: './test',
   use: {
-    // הכתובת של השרת המקומי שה-Action יקים
-    baseURL: 'http://127.0.0.1:4000', 
-    headless: true, // מריץ את הדפדפן ברקע בלי חלון גרפי כדי לחסוך זמן
+    // הכתובת של השרת
+    baseURL: process.env.BASE_URL || 'http://127.0.0.1:4000',
+    screenshot: 'only-on-failure',
   },
   webServer: {
-    // פקודה שמפעילה שרת סטטי על התיקייה שג'קיל בנה (_site)
-    command: 'npx serve -p 4000 _site', 
+    // הפקודה להפעלת השרת - Playwright יריץ אותה אוטומטית
+    command: 'npx serve -p 4000 _site',
     url: 'http://127.0.0.1:4000',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: !process.env.CI, // ב-GitHub (CI) הוא יפעיל מחדש, במחשב שלך הוא ישתמש בקיים
+    timeout: 120 * 1000,
   },
 });
